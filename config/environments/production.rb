121b1de95dd5abc6aeb8d5ec6f2818f87464895a
Brian Durbin <brian@rpmgreenville.com>
2016-03-21 14:43:47 +0000

    Generated user auth with devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Uofyou::Application.configure do
+  config.action_mailer.default_url_options = {host: 'uofyou-bdurb.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
